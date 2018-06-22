8d5f4df64f7d3fbccf2f251d7087acda9a5926b2
Jack Yu <llacque@gmail.com>
2018-06-22 16:59:35 +0800

    clirad-lw {'n2o': 3.2e-07} band[9] trp
    atmpro: trp
    
    band: [9]
    
    commitnumber: de5d43e
    
    molecule: {'n2o': 3.2e-07}
    
    tsfc: 300

---
+++ clirad/n2o_3.2e-07/band_9/atmpro_trp/cliradlw_de5d43e/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'trp', 'band': [9], 'commitnumber': 'de5d43e', 'molecule': {'n2o': 3.2e-07}, 'tsfc': 300}
+PARAM_LBLNEW = {'molecule': 'n2o', 'band': '7', 'commitnumber': '5014a19', 'vmin': 1215, 'vmax': 1380, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(1, 250), (500, 250)], 'ng_refs': [2, 2], 'ng_adju': [0, 0], 'klin': 2.22e-20, 'option_wgt_k': 1, 'wgt': [(0.6, 0.5), (0.6, 0.9)], 'w_diffuse': [(1.8, 1.66), (1.6, 1.8)], 'option_wgt_flux': 1, 'atmpro': 'trp', 'tsfc': 300, 'conc': 3.2e-07, 'option_compute_btable': 0, 'option_compute_ktable': 0}
