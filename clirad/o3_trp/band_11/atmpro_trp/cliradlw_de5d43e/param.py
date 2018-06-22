3b07db9fcc7ec79e30ecd0518196c939f45f142c
Jack Yu <llacque@gmail.com>
2018-06-22 16:59:33 +0800

    clirad-lw {'o3': 'atmpro'} band[11] trp
    atmpro: trp
    
    band: [11]
    
    commitnumber: de5d43e
    
    molecule: {'o3': 'atmpro'}
    
    tsfc: 300

---
+++ clirad/o3_trp/band_11/atmpro_trp/cliradlw_de5d43e/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'trp', 'band': [11], 'commitnumber': 'de5d43e', 'molecule': {'o3': 'atmpro'}, 'tsfc': 300}
+PARAM_LBLNEW = {'molecule': 'o3', 'band': '9', 'commitnumber': '5014a19', 'vmin': 1900, 'vmax': 3000, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(1, 250), (50, 250)], 'ng_refs': [2, 5], 'ng_adju': [0, 0], 'klin': 2e-20, 'option_wgt_k': 1, 'wgt': [(0.3, 0.4), (0.5, 0.6, 0.7, 0.85, 0.9)], 'w_diffuse': [(1.55, 1.55), (1.55, 1.55, 1.55, 1.55, 1.8)], 'option_wgt_flux': 1, 'atmpro': 'trp', 'tsfc': 300, 'conc': None, 'option_compute_btable': 0, 'option_compute_ktable': 0}
