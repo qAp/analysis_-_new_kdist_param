2444286f5f237c71d9b5fa7398683da804a835e3
Jack Yu <llacque@gmail.com>
2018-06-22 10:35:22 +0800

    clirad-lw {'co2': 0.0004} band[4] trp
    atmpro: trp
    
    band: [4]
    
    commitnumber: 4198f95
    
    molecule: {'co2': 0.0004}
    
    tsfc: 300

---
+++ clirad/co2_0.0004/band_4/atmpro_trp/cliradlw_4198f95/param.py
@@ -0,0 +1,2 @@
+PARAM = {'atmpro': 'trp', 'band': [4], 'commitnumber': '4198f95', 'molecule': {'co2': 0.0004}, 'tsfc': 300}
+PARAM_LBLNEW = {'molecule': 'co2', 'band': '3b', 'commitnumber': 'a06b618', 'vmin': 620, 'vmax': 720, 'dv': 0.001, 'nv': 1000, 'ref_pts': [(1, 250), (10, 250)], 'ng_refs': [5, 2], 'ng_adju': [0, 0], 'klin': 0, 'option_wgt_k': 1, 'wgt': [(0, 0.6, 0.5, 0.7, 0.8), (0.8, 0.7)], 'w_diffuse': [(1.66, 1.66, 1.66, 1.66, 1.66), (1.66, 1.66)], 'option_wgt_flux': 1, 'atmpro': 'trp', 'tsfc': 300, 'conc': 0.0004, 'option_compute_btable': 0, 'option_compute_ktable': 0}
