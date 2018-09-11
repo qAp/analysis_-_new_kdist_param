726df747ffaa0893c1902546c1295e995b4f999e
Jack Yu <llacque@gmail.com>
2018-09-11 13:49:48 +0800

    clirad-lw {'h2o': 'atmpro', 'co2': 0.0008} band[6] mls
    commitnumber: 523cbb7
    
    molecule: {'h2o': 'atmpro', 'co2': 0.0008}
    
    band: [6]
    
    atmpro: mls
    
    tsfc: 294

---
+++ clirad/h2o_mls_co2_0.0008/band_6/atmpro_mls/cliradlw_523cbb7/param.py
@@ -0,0 +1,2 @@
+PARAM = {'commitnumber': '523cbb7', 'molecule': {'h2o': 'atmpro', 'co2': 0.0008}, 'band': [6], 'atmpro': 'mls', 'tsfc': 294}
+PARAM_LBLNEW = {'atmpro': 'mls', 'band': '4', 'commitnumber': 'a22ab94', 'dv': 0.001, 'molecule': {'co2': 0.0008, 'h2o': 'atmpro'}, 'nv': 1000, 'tsfc': 294}
