8bacf758a0f9fb4f28a86f6382534e548d685bcf
Jack Yu <llacque@gmail.com>
2018-09-14 15:30:46 +0800

    {'h2o': 'atmpro', 'co2': 0.0004, 'n2o': 6.4e-07} band3a trp
    commitnumber: a22ab94
    
    molecule: {'h2o': 'atmpro', 'co2': 0.0004, 'n2o': 6.4e-07}
    
    band: 3a
    
    nv: 1000
    
    dv: 0.001
    
    atmpro: trp
    
    tsfc: 300

---
+++ lblnew/h2o_atmpro_co2_0.0004_o3_0_n2o_6.4e-07_ch4_0_o2_0/band03a_wn_540_620/nv_1000/dv_0.001/crd_a22ab94/atmpro_trp/param.py
@@ -0,0 +1,2 @@
+DIR_FORTRAN = '/chia_cluster/home/jackyu/radiation/crd/LW/examples/separate_g_groups/study__lblnew_g1_threshold/h2o_atmpro_co2_0.0004_o3_0_n2o_6.4e-07_ch4_0_o2_0/band03a_wn_540_620/nv_1000/dv_0.001/crd_a22ab94/atmpro_trp'
+PARAM = {'commitnumber': 'a22ab94', 'molecule': {'h2o': 'atmpro', 'co2': 0.0004, 'n2o': 6.4e-07}, 'band': '3a', 'nv': 1000, 'dv': 0.001, 'atmpro': 'trp', 'tsfc': 300}
