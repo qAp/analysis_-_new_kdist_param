7ff741bb1bec01403cbb5794e341d34720779cfc
Jack Yu <llacque@gmail.com>
2018-09-21 10:53:39 +0800

    clirad-lw {'ch4': 1.8e-06} band[9] trp
    commitnumber: 523cbb7
    
    molecule: {'ch4': 1.8e-06}
    
    band: [9]
    
    atmpro: trp
    
    tsfc: 300

---
+++ clirad/ch4_1.8e-06/band_9/atmpro_trp/cliradlw_523cbb7/param.py
@@ -0,0 +1,2 @@
+PARAM = {'commitnumber': '523cbb7', 'molecule': {'ch4': 1.8e-06}, 'band': [9], 'atmpro': 'trp', 'tsfc': 300}
+PARAM_LBLNEW = {'atmpro': 'trp', 'band': '7', 'commitnumber': '5014a19', 'conc': 1.8e-06, 'dv': 0.001, 'klin': 2e-21, 'molecule': 'ch4', 'ng_adju': [0, 0], 'ng_refs': [2, 3], 'nv': 1000, 'option_compute_btable': 0, 'option_compute_ktable': 0, 'option_wgt_flux': 1, 'option_wgt_k': 1, 'ref_pts': [(1, 250), (500, 250)], 'tsfc': 300, 'vmax': 1380, 'vmin': 1215, 'w_diffuse': [(1.66, 1.66), (1.66, 1.66, 1.66)], 'wgt': [(0.7, 0.7), (0.4, 0.6, 0.75)]}
