67a4ecd07f3984b50f4f6f3ca2af246f64158a7f
Jack Yu <llacque@gmail.com>
2018-12-06 09:04:50 +0800

    clirad-lw {'n2o': 3.2e-07} band[3] mls
    commitnumber: 1013f91
    
    band: [3]
    
    molecule: {'n2o': 3.2e-07}
    
    atmpro: mls
    
    tsfc: 294

---
+++ clirad/n2o_3.2e-07/band_3/atmpro_mls/cliradlw_1013f91/param.py
@@ -0,0 +1,2 @@
+PARAM = {'commitnumber': '1013f91', 'band': [3], 'molecule': {'n2o': 3.2e-07}, 'atmpro': 'mls', 'tsfc': 294}
+PARAM_LBLNEW = {'atmpro': 'mls', 'band': '3a', 'commitnumber': '5014a19', 'conc': 3.2e-07, 'dv': 0.001, 'klin': 2.22e-20, 'molecule': 'n2o', 'ng_adju': [0, 0], 'ng_refs': [1, 2], 'nv': 1000, 'option_compute_btable': 0, 'option_compute_ktable': 0, 'option_wgt_flux': 1, 'option_wgt_k': 1, 'ref_pts': [[1, 250], [500, 250]], 'tsfc': 294, 'vmax': 620, 'vmin': 540, 'w_diffuse': [[1.8], [1.66, 1.8]], 'wgt': [[0.9], [0.5, 0.95]]}
