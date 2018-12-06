42f2c154dceedab3270402223286c141acdc93fe
Jack Yu <llacque@gmail.com>
2018-12-06 11:43:36 +0800

    clirad-lw {'co2': 0.0004} band[11] mls
    commitnumber: 1013f91
    
    band: [11]
    
    molecule: {'co2': 0.0004}
    
    atmpro: mls
    
    tsfc: 294

---
+++ clirad/co2_0.0004/band_11/atmpro_mls/cliradlw_1013f91/param.py
@@ -0,0 +1,2 @@
+PARAM = {'commitnumber': '1013f91', 'band': [11], 'molecule': {'co2': 0.0004}, 'atmpro': 'mls', 'tsfc': 294}
+PARAM_LBLNEW = {'atmpro': 'mls', 'band': '9', 'commitnumber': 'a22ab94', 'conc': 0.0004, 'dv': 0.001, 'klin': 6.5e-24, 'molecule': 'co2', 'ng_adju': [0, 0], 'ng_refs': [3, 3], 'nv': 1000, 'option_compute_btable': 0, 'option_compute_ktable': 0, 'option_wgt_flux': 1, 'option_wgt_k': 1, 'ref_pts': [[1, 250], [50, 250]], 'tsfc': 294, 'vmax': 3000, 'vmin': 1900, 'w_diffuse': [[1.66, 1.66, 1.75], [1.75, 1.6, 1.85]], 'wgt': [[0.7, 0.8, 0.7], [0.8, 0.7, 0.8]]}
