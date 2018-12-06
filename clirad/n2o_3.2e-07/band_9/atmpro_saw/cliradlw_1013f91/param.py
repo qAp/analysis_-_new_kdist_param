3c869d6f224b328077173d3086db6de55e4c1ab7
Jack Yu <llacque@gmail.com>
2018-12-06 11:35:20 +0800

    clirad-lw {'n2o': 3.2e-07} band[9] saw
    commitnumber: 1013f91
    
    band: [9]
    
    molecule: {'n2o': 3.2e-07}
    
    atmpro: saw
    
    tsfc: 257

---
+++ clirad/n2o_3.2e-07/band_9/atmpro_saw/cliradlw_1013f91/param.py
@@ -0,0 +1,2 @@
+PARAM = {'commitnumber': '1013f91', 'band': [9], 'molecule': {'n2o': 3.2e-07}, 'atmpro': 'saw', 'tsfc': 257}
+PARAM_LBLNEW = {'atmpro': 'saw', 'band': '7', 'commitnumber': '5014a19', 'conc': 3.2e-07, 'dv': 0.001, 'klin': 2.22e-20, 'molecule': 'n2o', 'ng_adju': [0, 0], 'ng_refs': [2, 2], 'nv': 1000, 'option_compute_btable': 0, 'option_compute_ktable': 0, 'option_wgt_flux': 1, 'option_wgt_k': 1, 'ref_pts': [[1, 250], [500, 250]], 'tsfc': 257, 'vmax': 1380, 'vmin': 1215, 'w_diffuse': [[1.8, 1.66], [1.6, 1.8]], 'wgt': [[0.6, 0.5], [0.6, 0.9]]}
